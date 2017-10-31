92a764d073a0ba2e07857dc071aa2703ab67ddc7
keith(mac) <ka33145@googlemail.com>
2017-10-31 18:23:33 +0000

    Added shop and spec

---
+++ ruby/spec/shop_spec.rb
@@ -0,0 +1,13 @@
+require 'shop'
+
+describe Shop do
+
+  describe '#update_quality' do
+    it 'does not change the name' do
+      items = [Item.new('foo', 0, 0)]
+      Shop.new(items).update_quality()
+      expect(items[0].name).to eq 'foo'
+    end
+  end
+
+end
