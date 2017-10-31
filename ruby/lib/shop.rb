609cdbfd7767794506dc35576f9507a9a4e8034e
keith(mac) <ka33145@googlemail.com>
2017-10-31 18:55:00 +0000

    Split shop and item into seprate file

--- ruby/lib/shop.rb
+++ ruby/lib/shop.rb
@@ -4,65 +4,65 @@ class Shop
     @items = items
   end
 
+  MIN_QUALITY = 0
+  MAX_QUALITY = 50
+
   def update_quality()
     @items.each do |item|
+      # Not Aged Brie and  Backstage passes
       if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
           if item.name != 'Sulfuras, Hand of Ragnaros'
             item.quality = item.quality - 1
           end
         end
-      else
-        if item.quality < 50
+      else # Brie or Backstage passes
+        if item.quality < MAX_QUALITY
           item.quality = item.quality + 1
+          # Backstage passes only
           if item.name == 'Backstage passes to a TAFKAL80ETC concert'
             if item.sell_in < 11
-              if item.quality < 50
+              if item.quality < MAX_QUALITY
                 item.quality = item.quality + 1
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
+              if item.quality < MAX_QUALITY
                 item.quality = item.quality + 1
               end
             end
           end
+          # End of Backstage
         end
       end
+      # Not Sulfuras
       if item.name != 'Sulfuras, Hand of Ragnaros'
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
+        # Not Age Brie
         if item.name != 'Aged Brie'
+          # Not Backstage
           if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
+            if item.quality > MIN_QUALITY
+              # Not Sulfur
               if item.name != 'Sulfuras, Hand of Ragnaros'
                 item.quality = item.quality - 1
               end
+              # End of not sulfur, Backstage, Not Age Brie
             end
+            #Backstage Only
           else
             item.quality = item.quality - item.quality
           end
+        # Age Brie only
         else
-          if item.quality < 50
+          if item.quality < MAX_QUALITY
             item.quality = item.quality + 1
           end
         end
       end
     end
-  end
-end
-
-class Item
-  attr_accessor :name, :sell_in, :quality
-
-  def initialize(name, sell_in, quality)
-    @name = name
-    @sell_in = sell_in
-    @quality = quality
-  end
-
-  def to_s()
-    "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
